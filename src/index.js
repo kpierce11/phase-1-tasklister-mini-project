c0d8acb6e391f907b8ce12a69d81ed0cbd207b0e
Keira Pierce <kpierce1@samford.edu>
2024-07-02 05:55:10 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      const taskDescription = document.getElementById("new-task-description").value;
+  
+      if (taskDescription.trim() === "") {
+        alert("Please enter a task description.");
+        return;
+      }
+      
+      const taskItem = document.createElement("li")
+      taskItem.textContent = taskDescription;
+  
+      taskList.appendChild(taskItem);
+  
+      form.reset();
+    });
+  });
+  
 });
